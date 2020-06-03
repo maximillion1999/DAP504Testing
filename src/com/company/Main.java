d51c13f234f2c8f2c2835e32f7044a94db18332f
Roger Holden <r.holden@chi.ac.uk>
2020-06-03 11:33:39 +0100

    comment added in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("this is a test");
         Player thePlayer = new Player("Donald", "Duck");
         System.out.println("The Player first name is: "+thePlayer.firstName+". "+"The Player Last Name is: "+thePlayer.lastName+".");
+        // roger has made a comment
     }
 }
